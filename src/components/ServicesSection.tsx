e3792c69f68736a9d7b5ff4df88ed6161058f47b
Nimely111 <samuelehnimely@gmail.com>
2025-02-27 02:08:28 +0000

    add services into an array

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react"
+
+const services = [
+    "Academic Advisement",
+    "Career Counseling",
+    "Assessment Reporting",
+    "Personal Counseling",
+    "Therapeutic Services",
+    "Training",
+    "Outreach",
+    "Consultation",
+    "Crisis Intervention",
+    "Referral",
+];
+
 const ServicesSection = () => {
+
   return (
     <div>
       
