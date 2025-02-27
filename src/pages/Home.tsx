dd6757b14044fa75b323c16bca1945d69a53e939
Nimely111 <samuelehnimely@gmail.com>
2025-02-27 02:33:39 +0000

    add components info for the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import Navbar from "../components/Navbar";
+import ServicesSection from "../components/ServicesSection";
+
 const Home = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <header className="text-center p-10">
+        <h1 className="text-3xl font-bold">Welcome to SA2C3</h1>
+        <p className="mt-2 text-gray-600">Explore our services to support your academic and career journey.</p>
+      </header>
+      <ServicesSection />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
