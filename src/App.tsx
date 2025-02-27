5407de5f26da7669a9e87c7a75fdb41b3f61539d
Nimely111 <samuelehnimely@gmail.com>
2025-02-27 01:29:49 +0000

    add department name to page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
     <>
       <div >
         <Header />
-          <div className="h-36 flex items-center justify-center mt-36">
+          <div className="h-36 flex items-center justify-center mt-20">
           <img src={ulLogo} alt="ul logo" />
           </div>
       </div>
-      <h1>STUDENT ACADEMIC ADVISEMENT AND CAREER COUNCELING CENTER</h1>
-      <p>Welcome to the University of Liberia Student Academic Advisory and Career Counseling Center (UL-SA <sup>2</sup>C<sup>3</sup>)</p>
+      <h1 className='text-center font-medium mt-10'>STUDENT ACADEMIC ADVISEMENT AND CAREER COUNCELING CENTER</h1>
+      <p className='text-center font-medium mt-10'>Welcome to the University of Liberia Student Academic Advisory and Career Counseling Center (UL-SA <sup>2</sup>C<sup>3</sup>)</p>
       <Footer />
 
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div>
-      <h1 className="text-center font-bold text-5xl">Header</h1>
+      <h1 className="text-center font-bold text-5xl">Student Academic Advisement and Career Counceling Center</h1>
     </div>
   )
 }
