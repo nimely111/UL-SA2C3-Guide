c66d28d4ba5485952ac1cc22131371d7aa79952b
Nimely111 <samuelehnimely@gmail.com>
2025-03-02 00:18:12 +0000

    change the headings color to red following the rightful color scheme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,12 +7,12 @@ const Home = () => {
     <div className="min-h-screen bg-gray-100">
       <Navbar />
       <header className="text-center p-10">
-        <h1 className="text-3xl font-bold text-gray-700">Welcome to UL Student Academic Advisory and Career Counseling Center</h1>
-        <h2 className="text-2xl font-bold text-gray-700">(UL-SA <sup>2</sup>C<sup>3</sup>)</h2>
+        <h1 className="text-3xl font-bold text-red-700">Welcome to UL Student Academic Advisory and Career Counseling Center</h1>
+        <h2 className="text-2xl font-bold text-red-700">(UL-SA <sup>2</sup>C<sup>3</sup>)</h2>
         <p className="mt-2 text-gray-600">Explore our services to support your academic and career journey.</p>
       <img className="h-30 rounded-4xl object-center" src={ulLogo} alt="university of liberia logo" />
       </header>
-      <ServicesSection />
+      {/* <ServicesSection /> */}
     </div>
   );
 };
