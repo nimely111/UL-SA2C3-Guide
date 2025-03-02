25167614f25376bbcfa0cb55ac4d5d3a0467daa5
Nimely111 <samuelehnimely@gmail.com>
2025-03-02 00:11:51 +0000

    add styling to the achronmy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <Navbar />
       <header className="text-center p-10">
         <h1 className="text-3xl font-bold">Welcome to UL Student Academic Advisory and Career Counseling Center</h1>
-        <h2>(UL-SA <sup>2</sup>C<sup>3</sup>)</h2>
+        <h2 className="text-2xl font-bold">(UL-SA <sup>2</sup>C<sup>3</sup>)</h2>
         <p className="mt-2 text-gray-600">Explore our services to support your academic and career journey.</p>
       <img className="h-30 rounded-4xl object-center" src={ulLogo} alt="university of liberia logo" />
       </header>
