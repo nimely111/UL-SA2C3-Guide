89bc0ca1aa6026518673d08883d53dc17507feeb
Nimely111 <samuelehnimely@gmail.com>
2025-03-06 13:59:57 +0000

    add a padding to the home component header, to maintain the normal document flow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <header className="text-center p-10">
+      <header className="text-center p-10 pt-24">
         <h1 className="text-3xl font-bold text-red-700">Welcome to UL Student Academic Advisory and Career Counseling Center</h1>
         <h2 className="text-2xl font-bold text-red-700">(UL-SA <sup>2</sup>C<sup>3</sup>)</h2>
         <p className="mt-2 text-blue-900 text-lg">Explore our services to support your academic and career journey.</p>
