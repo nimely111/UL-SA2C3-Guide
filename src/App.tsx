e721b532d40b0575fe4c0ba442dc3eab8f9a4258
Nimely111 <samuelehnimely@gmail.com>
2025-03-06 14:39:43 +0000

    add staff component to the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Staff from "./pages/Staff";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
 
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/staff" element={<Staff />} />        
         <Route path="/contact" element={<Contact />} />        
         <Route path="/services" element={<Services />} />
       </Routes>
