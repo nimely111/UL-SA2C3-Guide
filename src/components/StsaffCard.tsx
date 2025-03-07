d51ebd00c9744acd09a6f494f8bf5be336c36636
Nimely111 <samuelehnimely@gmail.com>
2025-03-07 15:14:08 +0000

    add contents and assets to the staff contact component

--- src/components/StsaffCard.tsx
+++ src/components/StsaffCard.tsx
@@ -1,8 +1,26 @@
 const StaffCard = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+        return (
+            <article className="contact-card">
+                <img
+                    src="../assets/profile1.jpg"
+                    alt="Photo of Mr. Whiskerson"
+                />
+                <h3>Mr. Whiskerson</h3>
+                <div className="info-group">
+                    <img
+                        src="./images/phone-icon.png"
+                        alt="phone icon"
+                    /> 
+                    <p>(212) 555-1234</p>
+                </div>
+                <div className="info-group">
+                    <img
+                        src="./images/mail-icon.png"
+                        alt="mail icon"
+                    />
+                    <p>mr.whiskaz@catnap.meow</p>
+                </div>
+            </article>
+        )
+    }
 export default StaffCard;
