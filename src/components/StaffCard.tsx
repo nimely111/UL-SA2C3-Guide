c32a16f6c98496960649e9c2c5e0cd924d0a7196
Nimely111 <samuelehnimely@gmail.com>
2025-03-07 16:01:10 +0000

    import maiil and phone for staff card component

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -1,6 +1,8 @@
 import profile1 from "../assets/profile1.jpg"
 import profile2 from "../assets/profile2.jpg"
 import profile3 from "../assets/profile3.jpg"
+import phone from "../assets/phone.PNG"
+import mail from "../assets/mail.PNG"
 const StaffCard = () => {
         return (
             <article className="contact-card">
@@ -8,17 +10,17 @@ const StaffCard = () => {
                     src={profile1}
                     alt="Photo of Mr. Whiskerson"
                 />
-                <h3>Mr. Whiskerson</h3>
+                <h3>Dr. Al-Hassan Conteh</h3>
                 <div className="info-group">
                     <img
-                        src="./images/phone-icon.png"
+                        src={phone}
                         alt="phone icon"
                     /> 
                     <p>(212) 555-1234</p>
                 </div>
                 <div className="info-group">
                     <img
-                        src="./images/mail-icon.png"
+                        src={mail}
                         alt="mail icon"
                     />
                     <p>mr.whiskaz@catnap.meow</p>
