c20947bba6ce8abea59e13f15b754acb1073a4aa
Nimely111 <samuelehnimely@gmail.com>
2025-03-07 18:00:48 +0000

    import mail and phone logos into the staffcard component

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -1,3 +1,5 @@
+import mail_logo from "../assets/mail.jpg"
+import phone_logo from "../assets/phone.jpg"
 const StaffCard = (props) => {
         return (
             <article className="contact-card">
@@ -8,14 +10,14 @@ const StaffCard = (props) => {
                 <h3>{props.name}</h3>
                 <div className="info-group">
                     <img
-                        src="phone"
+                        src={phone_logo}
                         alt="phone icon"
                     /> 
                     <p>{props.phone}</p>
                 </div>
                 <div className="info-group">
                     <img
-                        src="email"
+                        src={mail_logo}
                         alt="mail icon"
                     />
                     <p>{props.email}</p>
